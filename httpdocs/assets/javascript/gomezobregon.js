a4a6f8f28ae49ef7a0160ef121097811ace4c067
Jaime Gómez-Obregón <jaime@gomezobregon.com>
2020-11-04 14:29:53 +0100

    Carga algunos contenidos más y avanza un poco en la funcionalidad y el diseño

--- httpdocs/assets/javascript/gomezobregon.js
+++ httpdocs/assets/javascript/gomezobregon.js
@@ -2,29 +2,42 @@ import { posts } from './posts.js?1'
 
 ((menu, contents) => {
 
-	const nav = document.querySelector(menu)
+    const nav = document.querySelector(menu)
 
-	const items = Object.entries(posts).map(([slug, post]) => `
+    const options = {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+    }
+
+    const items = Object.entries(posts).map(([slug, post]) => `
         <li>
-            <a href="/posts/${slug}">
+            <a href="/${slug}">
                 ${post.title}
+                <time>${new Date(post.date).toLocaleDateString('es-ES', options)}</time>
             </a>
         </li>
-	`)
+    `)
 
-	nav.innerHTML = `
-		<ol>
-			${items.join('')}
-		</ol>
-	`
+    nav.innerHTML = `
+        <ol>
+            ${items.join('')}
+        </ol>
+    `
 
-	nav.addEventListener('click', async event => {
-		document.querySelector('head base').remove()
-		document.head.innerHTML += `<base href="/posts/la-locomotora-de-praga/">`
-		event.preventDefault()
-		const response = await fetch(`${event.target.href}/index.html`)
-		const content = await response.text()
-		document.querySelector(contents).innerHTML = content
-	})
+    nav.addEventListener('click', async event => {
+        const a = event.target.closest('a')
+        if (!a) {
+            return
+        }
+
+        event.preventDefault()
+        document.querySelector('head base').remove()
+        document.head.innerHTML += `<base href="/posts/la-locomotora-de-praga/">`
+        const url = new URL(a.href)
+        const response = await fetch(`/posts${url.pathname}/index.html`)
+        const content = await response.text()
+        document.querySelector(contents).innerHTML = content
+    })
 
 })('body > nav', 'main')
--- httpdocs/assets/javascript/posts.js
+++ httpdocs/assets/javascript/posts.js
@@ -1,27 +1,362 @@
 export const posts = {
-    "la-locomotora-de-praga": {
-        title: "La locomotora de Praga",
-        date: "2006-08-10 20:28:00",
-        tags: ["general"],
-    },
-    "las-noticias-son-para-la-mente-lo-que-la-comida-basura-para-el-cuerpo": {
-        title: "Las noticias son para la mente lo que la comida basura para el cuerpo",
-        date: "2019-01-28 22:15:00",
-        tags: [],
-    },
-    "me-entrevistan-en-el-heraldo-de-colombia": {
-        title: "Me entrevistan en El Heraldo de Colombia",
-        date: "",
-        tags: [],
-    },
-    "fighting-email": {
-        title: "Fighting email",
-        date: "",
-        tags: [],
-    },
     "on-the-verge-of-a-new-era-freedom-tsunami-or-schemed-alienation": {
         title: "On the verge of a new era: freedom tsunami or schemed alienation?",
         date: "",
         tags: [],
+    },
+    "las-noticias-son-para-la-mente-lo-que-la-comida-basura-para-el-cuerpo": {
+        title: "Las noticias son para la mente lo que la comida basura para el cuerpo",
+        date: "2019-01-28 22:15",
+        tags: ["General"],
+    },
+    "me-entrevistan-en-el-heraldo-de-colombia": {
+        title: "Me entrevistan en El Heraldo de Colombia",
+        date: "2012-03-31 00:07",
+        tags: ["General"],
+    },
+    "fighting-email": {
+        title: "Fighting email",
+        date: "2012-01-16 13:26",
+        tags: ["General"],
+    },
+    "dimitrij-email-cz": {
+        title: "dimitrij@email.cz",
+        date: "2011-02-06 08:04",
+        tags: ["General"],
+    },
+    "terrorismo": {
+        title: "Terrorismo",
+        date: "2010-03-22 23:39",
+        tags: ["General"],
+    },
+    "fe": {
+        title: "Fe",
+        date: "2009-12-12 16:06",
+        tags: ["Dios ha muerto"],
+    },
+    "intelligence": {
+        title: "Intelligence",
+        date: "2009-02-17 01:14",
+        tags: ["General"],
+    },
+    "downshifting": {
+        title: "Downshifting",
+        date: "2009-02-16 00:25",
+        tags: ["General"],
+    },
+    "feynman-on-religion": {
+        title: "Feynman on religion",
+        date: "2009-02-05 03:48",
+        tags: ["General"],
+    },
+    "ha-muerto-mikel-laboa": {
+        title: "Ha muerto Mikel Laboa",
+        date: "2008-12-02 01:03",
+        tags: ["General"],
+    },
+    "can-we-have-our-ball-back": {
+        title: "Can we have our ball back?",
+        date: "2008-11-24 23:16",
+        tags: ["General"],
+    },
+    "correo-electronico": {
+        title: "Correo electrónico",
+        date: "2008-10-28 02:29",
+        tags: ["General"],
+    },
+    "microsoft-word-ese-gran-desconocido": {
+        title: "Microsoft Word: ese gran desconocido",
+        date: "2008-10-07 08:49",
+        tags: ["General"],
+    },
+    "ernst-rutherford-hablando-sobre-niels-bohr": {
+        title: "Ernst Rutherford hablando sobre Niels Bohr",
+        date: "2008-09-15 23:59",
+        tags: ["General"],
+    },
+    "safari": {
+        title: "Safari",
+        date: "2008-07-12 23:34",
+        tags: ["General"],
+    },
+    "preguntas-de-un-obrero-que-lee": {
+        title: "Preguntas de un obrero que lee",
+        date: "2008-02-15 01:12",
+        tags: ["General"],
+    },
+    "por-que-tienen-las-horas-sesenta-minutos": {
+        title: "¿Por qué tienen las horas sesenta minutos?",
+        date: "2008-02-02 05:00",
+        tags: ["General"],
+    },
+    "asignatura-de-religion": {
+        title: "¿Asignatura de religión?",
+        date: "2008-01-28 23:42",
+        tags: ["General"],
+    },
+    "stockholm-syndrome": {
+        title: "Stockholm syndrome",
+        date: "2008-01-13 15:18",
+        tags: ["General"],
+    },
+    "conciencia": {
+        title: "Conciencia",
+        date: "2008-01-13 02:49",
+        tags: ["General"],
+    },
+    "la-identidad-de-euler": {
+        title: "La identidad de Euler",
+        date: "2008-01-06 21:05",
+        tags: ["General"],
+    },
+    "aprender-y-olvidar": {
+        title: "Aprender y olvidar",
+        date: "2008-01-02 13:25",
+        tags: ["Idiomas"],
+    },
+    "neue-slowenische-kunst": {
+        title: "Neue Slowenische Kunst",
+        date: "2007-12-30 18:05",
+        tags: ["General"],
+    },
+    "el-hombre-mas-sabio-del-mundo": {
+        title: "El hombre más sabio del mundo",
+        date: "2007-12-30 16:48",
+        tags: ["General"],
+    },
+    "copia-el-semanal-xl-a-stern": {
+        title: "¿Copia El Semanal XL a Stern?",
+        date: "2007-11-06 01:03",
+        tags: ["General"],
+    },
+    "nacionalismo-e-idioma": {
+        title: "Nacionalismo e idioma",
+        date: "2007-10-25 00:28",
+        tags: ["Idiomas"],
+    },
+    "otono-en-alemania": {
+        title: "Otoño en Alemania",
+        date: "2007-10-16 03:05",
+        tags: ["General"],
+    },
+    "me-debe-usted-cuarenta-francos": {
+        title: "Me debe usted cuarenta francos",
+        date: "2007-10-14 20:46",
+        tags: ["Literatura"],
+    },
+    "le-vent-nous-portera": {
+        title: "Le vent nous portera",
+        date: "2007-10-03 11:03",
+        tags: ["Idiomas", "Poesía"],
+    },
+    "ciencia-para-explicar-la-religion": {
+        title: "Ciencia para explicar la religión",
+        date: "2007-09-26 01:07",
+        tags: ["Dios ha muerto"],
+    },
+    "la-fiesta-de-la-democracia": {
+        title: "La fiesta de la democracia",
+        date: "2007-09-22 18:03",
+        tags: ["General"],
+    },
+    "dias-de-28-horas": {
+        title: "Días de 28 horas",
+        date: "2007-08-16 12:19",
+        tags: ["General"],
+    },
+    "que-listo-es": {
+        title: "Qué listo es",
+        date: "2007-05-11 20:25",
+        tags: ["General"],
+    },
+    "mucho-cuidado": {
+        title: "Mucho cuidado",
+        date: "2007-05-11 08:34",
+        tags: ["General"],
+    },
+    "ostalgia": {
+        title: "Ostalgia",
+        date: "2007-05-09 01:07",
+        tags: ["Idiomas", "Literatura", "Poesía"],
+    },
+    "fud": {
+        title: "FUD",
+        date: "2007-05-02 17:56",
+        tags: ["General"],
+    },
+    "rir": {
+        title: "Rir",
+        date: "2007-04-28 00:19",
+        tags: ["General"],
+    },
+    "gott-nein-kirche-nein": {
+        title: "Gott nein, Kirche nein",
+        date: "2007-04-27 23:33",
+        tags: ["Dios ha muerto"],
+    },
+    "la-leccion-del-adios": {
+        title: "La lección del adiós",
+        date: "2007-04-10 00:17",
+        tags: ["General"],
+    },
+    "10-preguntas-para-un-cristiano-inteligente": {
+        title: "10 Preguntas que todo cristiano inteligente debería plantearse",
+        date: "2007-04-06 18:01",
+        tags: ["Dios ha muerto"],
+    },
+    "la-donna-e-mobile": {
+        title: "La donna è mobile",
+        date: "2007-03-18 23:15",
+        tags: ["General"],
+    },
+    "quiero-toda-la-tierra": {
+        title: "Quiero toda la tierra",
+        date: "2007-03-18 20:50",
+        tags: ["General"],
+    },
+    "irgendwie-irgendwo-irgendwann": {
+        title: "Irgendwie, irgendwo, irgendwann",
+        date: "2007-03-15 01:14",
+        tags: ["General"],
+    },
+    "sonar-profundo": {
+        title: "Sonar profundo",
+        date: "2007-01-27 18:09",
+        tags: ["General"],
+    },
+    "siempre": {
+        title: "Siempre",
+        date: "2007-01-09 00:25",
+        tags: ["General"],
+    },
+    "der-wille-zur-macht": {
+        title: "Der Wille zur Macht",
+        date: "2007-01-08 01:09",
+        tags: ["Dios ha muerto", "Literatura"],
+    },
+    "besos": {
+        title: "Besos",
+        date: "2006-12-29 00:34",
+        tags: ["General"],
+    },
+    "sodade": {
+        title: "Sodade",
+        date: "2006-12-27 00:30",
+        tags: ["Idiomas", "Poesía"],
+    },
+    "si-quiero-conocer": {
+        title: "Si quiero conocer",
+        date: "2006-12-07 00:46",
+        tags: ["Literatura"],
+    },
+    "el-banquero-anarquista": {
+        title: "El banquero anarquista",
+        date: "2006-11-22 01:30",
+        tags: ["Literatura"],
+    },
+    "dura-dura-lex": {
+        title: "Dura, dura lex",
+        date: "2006-11-07 23:18",
+        tags: ["General"],
+    },
+    "ragioni-per-investire-in-italia": {
+        title: "Due grandi ragioni per investire in Italia",
+        date: "2006-10-31 00:32",
+        tags: ["General"],
+    },
+    "agua-y-aceite": {
+        title: "Agua y aceite",
+        date: "2006-10-22 23:40",
+        tags: ["General"],
+    },
+    "About \"inner revelations\"...": {
+        title: "about-inner-revelations",
+        date: "2006-09-21 15:42",
+        tags: ["Dios ha muerto"],
+    },
+    "pueblo-de-dios-nuevos-capitulos": {
+        title: "Pueblo de Dios, nuevos capítulos",
+        date: "2006-08-29 23:34",
+        tags: ["Dios ha muerto"],
+    },
+    "in-grog-we-trust": {
+        title: "In Grog we trust",
+        date: "2006-08-28 14:37",
+        tags: ["General"],
+    },
+    "la-locomotora-de-praga": {
+        title: "La locomotora de Praga",
+        date: "2006-08-10 20:28",
+        tags: ["general"],
+    },
+    "el-rio": {
+        title: "El río",
+        date: "2006-08-07 23:49",
+        tags: ["Literatura"],
+    },
+    "todas-as-cartas-de-amor": {
+        title: "Todas as cartas de amor",
+        date: "2006-08-07 20:32",
+        tags: ["Literatura", "Poesía"],
+    },
+    "languages": {
+        title: "Languages",
+        date: "2006-08-07 20:27",
+        tags: ["Idiomas"],
+    },
+    "ateismo": {
+        title: "Ateísmo",
+        date: "2006-07-09 01:02",
+        tags: ["Dios ha muerto"],
+    },
+    "medios": {
+        title: "Medios",
+        date: "2006-06-30 15:40",
+        tags: ["Literatura", "General"],
+    },
+    "navegar": {
+        title: "Navegar",
+        date: "2006-06-25 15:19",
+        tags: ["Literatura", "Dios ha muerto"],
+    },
+    "cuando-te-vieres-ante-la-luz-de-su-hermosura": {
+        title: "Cuando te vieres ante la luz de su hermosura",
+        date: "2006-06-24 21:56",
+        tags: ["Literatura"],
+    },
+    "el-sol-grande-en-la-era": {
+        title: "El Sol grande en la era...",
+        date: "2006-06-22 23:34",
+        tags: ["Poesía"],
+    },
+    "que-le-pedirias-al-presidente": {
+        title: "¿Qué le pedirías al Presidente?",
+        date: "2006-06-22 22:57",
+        tags: ["General"],
+    },
+    "el-lugar-maldito": {
+        title: "El lugar maldito",
+        date: "2006-06-16 22:07",
+        tags: ["Dios ha muerto", "Literatura"],
+    },
+    "esta-bien-esta-mal": {
+        title: "Está bien, está mal",
+        date: "2006-06-16 16:01",
+        tags: ["Dios ha muerto"],
+    },
+    "vinas-verdes-junto-al-mar": {
+        title: "Viñas verdes junto al mar",
+        date: "2006-06-11 20:43",
+        tags: ["Idiomas", "Poesía"],
+    },
+    "poemas-palindromicos-en-euskera": {
+        title: "Poemas palindrómicos en euskera",
+        date: "2006-06-11 16:23",
+        tags: ["Idiomas", "Poesía"],
+    },
+    "o-sentimento-do-mundo": {
+        title: "O sentimento do mundo",
+        date: "2006-06-11 14:46",
+        tags: ["Idiomas", "Poesía"],
     },
 }
