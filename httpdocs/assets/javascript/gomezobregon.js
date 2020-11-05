946412495bc0025ba4c0eafd0b6abd563b70cdcf
Jaime Gómez-Obregón <jaime@gomezobregon.com>
2020-11-05 15:35:10 +0100

    Añade algo de funcionalidad desordenada, que habrá que refactorizar

--- httpdocs/assets/javascript/gomezobregon.js
+++ httpdocs/assets/javascript/gomezobregon.js
@@ -1,14 +1,46 @@
 import { posts } from './posts.js?1'
 
-((menu, contents) => {
-
-    const nav = document.querySelector(menu)
-
     const options = {
         day: 'numeric',
         month: 'long',
         year: 'numeric',
     }
+
+const load = async (url, contents) => {
+    const slug = url.split('/')[1]
+    document.querySelector('head base').remove()
+    document.head.innerHTML += `<base href="/posts/${slug}/">`
+    const response = await fetch(`/posts/${slug}/index.html`)
+
+    if (!response.ok) {
+        console.log('404')
+    }
+    else {
+        document.body.classList.add('article')
+        const content = await response.text()
+        document.querySelector(contents).innerHTML = content
+
+        const header = document.createElement('header')
+
+        const h1 = document.querySelector(contents).querySelector('h1').innerHTML
+        const date = new Date(posts[slug].date).toLocaleDateString('es-ES', options)
+
+        header.innerHTML = `
+            <h1>${h1}</h1>
+            <time>${date}</time>
+        `
+
+        document.querySelector(contents).querySelector('h1').replaceWith(header)
+
+        const title = posts[slug].title
+        history.pushState({}, title, url)
+    }
+}
+
+((menu, contents) => {
+
+    const nav = document.querySelector(menu)
+
 
     const items = Object.entries(posts).map(([slug, post]) => `
         <li>
@@ -25,19 +57,30 @@ import { posts } from './posts.js?1'
         </ol>
     `
 
-    nav.addEventListener('click', async event => {
+    nav.addEventListener('click', event => {
         const a = event.target.closest('a')
         if (!a) {
             return
         }
 
-        event.preventDefault()
-        document.querySelector('head base').remove()
-        document.head.innerHTML += `<base href="/posts/la-locomotora-de-praga/">`
         const url = new URL(a.href)
-        const response = await fetch(`/posts${url.pathname}/index.html`)
-        const content = await response.text()
-        document.querySelector(contents).innerHTML = content
+
+        event.preventDefault()
+
+        load(url.pathname, contents)
     })
 
-})('body > nav', 'main')
+})('nav', 'main div')
+
+
+document.querySelector('button').addEventListener('click', event => {
+    document.body.classList.toggle('article')
+    // history.back()
+    history.pushState({}, '', '/')
+})
+
+const slug = document.location.pathname.split('/')[1]
+
+if (posts[slug]) {
+    load('/' + slug, 'main div')
+}
